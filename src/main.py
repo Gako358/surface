41169335840d78dbc899aaccb47e4774350df5ed
merrinx <gako358@outlook.com>
2022-02-26 14:15:33 +0100

    removed install

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 
 from pygit import Pygit as pg
-from confup import push, pull, install
+from confup import push, pull
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,13 +16,11 @@ def main():
     # push and pull local and remote config files
     parser_push = subparsers.add_parser("push", help="Push local cfg files to commit changes")
     parser_pull = subparsers.add_parser("pull", help="Pull remote files and update local")
-    parser_install = subparsers.add_parser("install", help="Setup all config files")
     
     parser_update.set_defaults(func=pg.run) 
 
     parser_push.set_defaults(func=push)
     parser_pull.set_defaults(func=pull)
-    parser_install.set_defaults(func=install)
 
     options = parser.parse_args()
     options.func(options)
